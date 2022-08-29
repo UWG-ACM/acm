d2ce48d40f13430491d9d248ad9fbb644aa52358
Alexander Ayers <alexayers2000@gmail.com>
2022-08-28 21:37:06 -0400

    Updated officer and staff pictures to scale with box.

--- components/Officers.js
+++ components/Officers.js
@@ -3,30 +3,30 @@ app.component("officers", {
   template: `
         <div id="acmOfficers">
             <div id="acmPresident">
-                <img src="images/alexander.jpg" alt="ACM President" width="150" height="150"/>
+                <img class="person" src="images/alexander.jpg" alt="ACM President" width="150" height="150"/>
                 <span class="acmPresidentDesc"><b>Alexander Ayers</b><br>ACM President</span><br>
                 <a href="mailto:aayers4@my.westga.edu">aayers4@my.westga.edu</a>
             </div>
             <div id="acmVicePresident">
-                <img src="images/shawn.jpg" alt="ACM Vice President" width="150" height="150"/>
+                <img class="person" src="images/shawn.jpg" alt="ACM Vice President" width="150" height="150"/>
                 <span class="acmVicePresidentDesc"><b>Shawn Carter</b><br>ACM Vice President</span><br>
                 <a href="mailto:sc00149@my.westga.edu">sc00149@my.westga.edu</a>
             </div>
 
             <div id=secretaryTreasurer>
-                <img src="images/Shadow.png" alt="ACM Secretary-Treasurer" width="150" height="150"/>
+                <img class="person" src="images/Shadow.png" alt="ACM Secretary-Treasurer" width="150" height="150"/>
                 <span class="acmSecretaryTreasurerDesc"><b>Alex DeCesare</b><br>ACM Treasurer</span><br>
                 <a href="mailto:adecesa1@my.westga.edu">adecesa1@my.westga.edu</a>
             </div>    
 
             <div id="acmOfficer1">
-                <img src="images/Shadow.png" alt="ACM Event Coordinator" width="150" height="150"/>
-                <span class="acmOfficer1Desc"><b>William Clark</b><br>ACM Technical Engineer</span><br>
+                <img class="person" src="images/Shadow.png" alt="ACM Event Coordinator" width="150" height="150"/>
+                <span class="acmOfficer1Desc"><b>William Clark</b><br>ACM Webmaster</span><br>
                 <a href="mailto:wclark7@my.westga.edu">wclark7@my.westga.edu</a>
             </div>
             
             <div id="acmOfficer2">
-                <img src="images/garrett.jpg" alt="ACM Event Coordinator" width="150" height="150"/>
+                <img class="person" src="images/garrett.jpg" alt="ACM Event Coordinator" width="150" height="150"/>
                 <span class="acmOfficer2Desc"><b>Garrett Brenner</b><br>ACM Event Coordinator</span><br>
                 <a href="mailto:gbrenne1@my.westga.edu">gbrenne1@my.westga.edu</a>
             </div>
