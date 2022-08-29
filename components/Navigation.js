4fad13b33d04163384a6efd611c8ff92a3c5212f
UWG-ACM <112016992+UWG-ACM@users.noreply.github.com>
2022-08-28 21:09:46 -0400

    Updated navbar to match index.html.

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,7 +2,7 @@ app.component("acm-nav", {
   /* html */
   template: `
     <nav id="navigation">
-        <a id="acmHomeTab" href="acmHomepage.html"> Home </a>
+        <a id="acmHomeTab" href="index.html"> Home </a>
         <a id="eventsTab" href="events.html"> Events </a>
         <a id="governanceTab" href="governance.html"> Governance </a>
     </nav>
